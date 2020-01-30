580b872a0fbb189ef003fdfd97f348ae7a51b814
Jefferson Santos <yuiti.santos@gmail.com>
2020-01-30 12:36:15 -0300

    Adicionando Middlewares

--- index.js
+++ index.js
@@ -1,10 +1,46 @@
 const express = require("express");
-
 const app = express();
 
 app.use(express.json());
 
 const projects = [];
+
+//Middlewares
+function checkIdExists(req, res, next) {
+  const { id } = req.params;
+
+  const index = projects.findIndex(element => element.id === id);
+  
+  if(index < 0 ) {
+    return res.status(400).json({ error: "ID informado não existe" });
+  }
+
+  req.index = index;
+
+  return next();
+}
+
+app.use((req, res, next) => {
+  console.count("Quantidade de requisições realizadas");
+
+  return next();
+})
+
+// Middleware: Opcional
+// Verifica se o 'title' foi informado no corpo da requisição
+function checkTitleExists(req, res, next) {
+  const { title } = req.body;
+
+  if(!title) {
+    return res.status(400).json({ error: "Title não informado" });
+  }
+
+  req.title = title;
+
+  return next();
+}
+
+// ROTAS
 
 // Listando todos os projetos e as tarefas
 app.get('/projects', (req, res) => {
@@ -13,46 +49,36 @@ app.get('/projects', (req, res) => {
 
 // Criando um novo projeto e suas tarefas
 app.post('/projects', (req, res) => {
-	const { id, title } = req.body;
+	const project = req.body;
 
-	projects.push({
-		id,
-		title,
-		tasks: []
-	})
+	projects.push(project)
 
 	return res.status(200).json(projects);
 });
 
-// Altera um projeto
-app.put('/projects/:id', (req, res) => {
-	const { id } = req.params;
-	const { title } = req.body;
-
-	const project = projects.find(element => element.id === id);
-
-	project.title = title;
+// Altera o título do projeto
+app.put('/projects/:id', checkIdExists, checkTitleExists, (req, res) => {
+  const { index, title } = req;
+  
+	projects[index].title = title;
 
 	return res.status(200).json(project);
 });
 
 // Deleta um projeto
-app.delete('/projects/:id', (req, res) => {
-	const { id } = req.params;
-	
-	const index = projects.findIndex(element => element.id === id);
+app.delete('/projects/:id', checkIdExists, (req, res) => {
+  const { index } = req;
 
 	projects.splice(index, 1);
 
 	return res.json();
 });
 
-// Adicionando uma tarefa no projeto
-app.post('/projects/:id/tasks', (req, res) => {
-	const { id } = req.params;
-	const { title } = req.body;
-
-	const project = projects.find(element => element.id === id);
+// Adciiona uma tarefa no projeto
+app.post('/projects/:id/tasks', checkIdExists, checkTitleExists, (req, res) => {
+  const { index, title } = req;
+  
+	const project = projects[index];
 
 	project.tasks.push(title);
 
